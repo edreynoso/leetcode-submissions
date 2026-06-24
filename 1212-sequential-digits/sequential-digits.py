ba3e00c43de71a94d061b63fc6e7e249c7d1e5ee
Edgar Reynoso <reynosoedgar276@gmail.com>
2026-06-24 10:04:10 -0700

    Time: 0 ms (100.00%) | Memory: 12.2 MB (91.30%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sequentialDigits(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: List[int]
+        """
+
+        c = "123456789"
+        a = []
+
+        for i in range(len(c)):
+            for j in range(i + 1, len(c) + 1):
+                curr = int(c[i:j])
+                if low <= curr <= high:
+                    a.append(curr)
+
+        a.sort()
+        return a
