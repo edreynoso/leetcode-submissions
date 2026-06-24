f520bfe8e3b6b21742d60b726ff6aef30e69e7f6
Edgar Reynoso <reynosoedgar276@gmail.com>
2026-06-24 09:05:19 -0700

    Time: 316 ms (42.65%) | Memory: 14.4 MB (68.24%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -0,0 +1,43 @@
+import heapq
+class Solution(object):
+    def suggestedProducts(self, products, searchWord):
+        """
+        :type products: List[str]
+        :type searchWord: str
+        :rtype: List[List[str]]
+        """
+
+        products.sort()
+
+        map = {}
+
+        res = []
+
+        for i in range(len(searchWord)):
+
+            search = searchWord[:i+1]
+
+            results = []
+
+            heapq.heapify(results)
+
+            for product in products:
+
+                if product.startswith(search):
+                    heapq.heappush(results, product)
+            
+            
+            level = []
+            for i in range(3):
+
+                if results:
+                    level.append(heapq.heappop(results))
+
+            res.append(level)
+
+        return res
+
+
+
+
+        
