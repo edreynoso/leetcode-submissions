811cee0b0361c217205019a808de7c4d550eec91
Edgar Reynoso <reynosoedgar276@gmail.com>
2026-07-07 21:29:10 -0700

    Time: 1 ms (49.66%) | Memory: 19.4 MB (45.09%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,56 +1,38 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        
+
         s = s.strip()
 
-        char_arr = list(s)
+        if not s:
+            return 0
 
-        digits = set()
+        MAX = 2 ** 31 -1
+        MIN = -(2**31)
 
-        for i in range(10):
+        idx = 0
+        number = 0
 
-            digits.add(str(i))
+        sign = 1
 
+        if s[idx] == "-":
+            sign = -1
+            idx +=1
+        elif s[idx] == "+":
+            idx +=1
 
-        def convert(arr):
+        while (idx < len(s) and s[idx].isdigit()):
 
-            sign = {"-": -1, "+": 1}
+            number = number * 10 + int(s[idx]) 
+            idx +=1
 
-            number = ""
+        
+        number *= sign
 
-            isNegative = 1
+        if number < MIN:
+            return MIN
 
-            prev = ""
-
-            for digit in arr:
-
-                if prev not in digits and digit not in digits and len(prev) > 0:
-                    break
-
-                if digit in sign and len(number) == 0:
-                    isNegative = sign[digit]
-                    prev = digit
-                    continue
-                
-                if digit == "0" and len(number) == 0 and isNegative < 0:
-                    prev = digit
-                    continue
-                
-                if digit not in digits:
-                    break
-
-                number += digit
-
-                prev = digit
-
-            return isNegative * int(number) if len(number) > 0 else 0
-
-        number = convert(char_arr)
-
-        if number < -(2 ** 31):
-            return -(2**31)
-
-        if number > 2** 31 -1:
-            return 2 **31 -1
+        if number > MAX:
+            return MAX
 
         return number
+        
