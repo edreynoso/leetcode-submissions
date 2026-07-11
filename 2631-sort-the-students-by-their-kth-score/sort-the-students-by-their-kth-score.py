966c52fcd63243c37c91ef26bc4f9ad5361da74e
Edgar Reynoso <reynosoedgar276@gmail.com>
2026-07-10 19:36:34 -0700

    Time: 0 ms (100.00%) | Memory: 24.9 MB (66.59%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,28 +1,6 @@
-import heapq
-class Solution(object):
-    def sortTheStudents(self, score, k):
-        """
-        :type score: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
         
-        scores_sorted = []
-
-        heapq.heapify(scores_sorted)
-
-        for i in range(len(score)):
-
-            heapq.heappush(scores_sorted, (-score[i][k], score[i]))
-        
-        for i in range(len(score)):
-
-            student = heapq.heappop(scores_sorted)
-
-            student= student[1]
-
-            score[i] = student
+        score.sort(key = lambda x: -x[k])
 
         return score
-
-        
